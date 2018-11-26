5602e4d918b9e6c5b7bc1008c2facebada736873
artlance <artlance.ru@gmail.com>
2018-11-26 10:44:38 +0200

    imp

--- sw.js
+++ sw.js
@@ -1,12 +1,7 @@
-
 'use strict';
-
 importScripts('sw-toolbox.js');
-
-toolbox.precache(["index.html", "css/style.css"]);
-
+toolbox.precache(["index.html", "css/style.css", "js/jquery.js", "js/library.js", "js/migrate.js", "js/script.js"]);
 toolbox.router.get('/upload/*', toolbox.cacheFirst);
-
 toolbox.router.get('/*', toolbox.networkFirst, {
-  networkTimeoutSeconds: 5
+    networkTimeoutSeconds: 5
 });
